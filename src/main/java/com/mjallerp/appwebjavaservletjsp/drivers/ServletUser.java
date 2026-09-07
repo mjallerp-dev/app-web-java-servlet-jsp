a5aa1c0eecd43cae99e4df6fdedc9b4729aa7af5
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-06 22:40:04 -0500

    feat(user): add servlet user class

---
+++ src/main/java/com/mjallerp/appwebjavaservletjsp/drivers/ServletUser.java
@@ -0,0 +1,8 @@
+package com.mjallerp.appwebjavaservletjsp.drivers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/User")
+public class ServletUser extends HttpServlet {
+}
